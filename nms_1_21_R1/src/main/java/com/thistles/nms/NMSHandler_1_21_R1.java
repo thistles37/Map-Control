7f26a288fce916e0249eee3f9d22f02e8ba85e7c
thistles37 <ultimatequalifo@gmail.com>
2024-06-17 13:21:20 +0200

    1.21 support

---
+++ nms_1_21_R1/src/main/java/com/thistles/nms/NMSHandler_1_21_R1.java
@@ -0,0 +1,24 @@
+package com.thistles.nms;
+
+import com.thistles.nmshandler.NMSHandler;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.saveddata.SavedData;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_21_R1.CraftWorld;
+
+import java.util.HashMap;
+
+public class NMSHandler_1_21_R1 implements NMSHandler {
+    private final CraftWorld world;
+
+    public NMSHandler_1_21_R1() {
+        this.world = (CraftWorld) Bukkit.getWorlds().get(0);
+    }
+
+    @Override
+    public void removeCache(int id) {
+        ServerLevel server = world.getHandle();
+        HashMap<String, SavedData> cache = (HashMap<String, SavedData>) server.getDataStorage().cache;
+        cache.remove("map_" + id);
+    }
+}
